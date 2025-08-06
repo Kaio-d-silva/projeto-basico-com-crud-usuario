01808cd28a9b729022e207b432f19a5e7aeb2bb8
Your Name <you@example.com>
2025-08-05 21:12:56 -0400

    alteração credenciais banco de dados

--- src/config/routes.ts
+++ src/config/routes.ts
@@ -4,6 +4,14 @@ import fg from 'fast-glob'
 export default (app: Express): void => {
   const router = Router()
   app.use('/api', router)
+
+  // fg.sync('**/src/routes/**').forEach(folder => {
+  //   fg.sync(`**/${folder}/**.ts`).map(async file => {
+  //     (await import(`../../${file}`)).default(router)
+  //   })
+  // })
+
+
   fg.sync('**/src/routes/**.ts').map(async file => {
     (await import(`../../${file}`)).default(router)
   })
