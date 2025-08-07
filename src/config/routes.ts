43433abd491d02282f8f03590dff4a564d0b611e
Your Name <you@example.com>
2025-08-06 21:43:53 -0400

    configuração

--- src/config/routes.ts
+++ src/config/routes.ts
@@ -5,14 +5,8 @@ export default (app: Express): void => {
   const router = Router()
   app.use('/api', router)
 
-  // fg.sync('**/src/routes/**').forEach(folder => {
-  //   fg.sync(`**/${folder}/**.ts`).map(async file => {
-  //     (await import(`../../${file}`)).default(router)
-  //   })
-  // })
-
-
   fg.sync('**/src/routes/**.ts').map(async file => {
     (await import(`../../${file}`)).default(router)
+    // console.log(file)
   })
 }
