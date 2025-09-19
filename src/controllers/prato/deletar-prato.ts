d147975759e8e6887cb582dcfe5079ff52401917
Your Name <you@example.com>
2025-09-18 21:02:34 -0400

    clone professor

--- src/controllers/prato/deletar-prato.ts
+++ src/controllers/prato/deletar-prato.ts
@@ -1,33 +1,31 @@
-import { Controller, HttpRequest,HttpResponse } from "../../interfaces";
+import { Controller, HttpRequest, HttpResponse } from "../../interfaces";
 import Prato from "../../models/prato-model";
 
-class DeletarPratoController implements Controller {
-    async handle(httpRequest:HttpRequest): Promise<HttpResponse>{
-        const { id } = httpRequest.params
-
+export default class DeletarPratoController implements Controller {
+    async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
         try {
-            const prato = await Prato.findByPk(id)
+            const { id } = httpRequest.params;
 
+            const prato = await Prato.findByPk(id);
 
-            if(!prato){
-                return{
+            if (!prato) {
+                return {
                     statusCode: 404,
-                    body: {error: "Prato não encontrado"}
-                }
-            }
-            await prato.destroy();
-            return{
-                statusCode: 204,
-                body: {message: "Prato deletado com sucesso"}
+                    body: { error: 'Prato não encontrado' }
+                };
             }
 
-            
-        } catch (error:any) {
-            return{
-                statusCode:500,
-                body: {error: error.message}
-            }
+            await prato.destroy();
+
+            return {
+                statusCode: 200,
+                body: { message: 'Prato deletado com sucesso' }
+            };
+        } catch (error: any) {
+            return {
+                statusCode: 500,
+                body: { error: error.message }
+            };
         }
     }
 }
-export default DeletarPratoController
--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
 // Bibliotecas
 import sequelize from "./database";
 import { ENV } from "./config/env";
-import { initializeDatabaseAndServer } from "./config/initializeDatabaseAndServer.ts";
+import { initializeDatabaseAndServer } from "./config/initializeDatabaseAndServer";
 
 // Função para iniciar o servidor em uma porta específica
 const startServer = async (port: number) => {
