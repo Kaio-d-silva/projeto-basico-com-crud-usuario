abef6c9d8e164898d53e3473089c7e50f381622c
Your Name <you@example.com>
2025-10-13 19:49:22 -0400

    atualizacao

--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -1,18 +1,5 @@
+import { Role } from '../enums/role';
 import {StatusPedido} from '../enums/status-pedido';
-
-export interface Controller {
-  handle: (httpRequest: HttpRequest) => Promise<HttpResponse>;
-}
-export interface HttpRequest {
-  body: any;
-  params: any;
-  pathParameters: any;
-  queryStringParameters: any;
-}
-export interface HttpResponse {
-  statusCode: number;
-  body: any;
-}
 
 export interface PedidoItemDTO {
   produtoId: string;
@@ -28,4 +15,38 @@ export interface CreatePedidoDTO {
 export interface UpdatePedidoDTO {
   itens?: PedidoItemDTO[];
   status?: StatusPedido;
+}
+
+export interface CriarUsuarioDTO {
+  nome: string;
+  email: string;
+  senha: string;
+  role: Role;
+}
+
+export interface EditarUsuarioDTO {
+  nome?: string;
+  email?: string;
+  senha?: string;
+  role?: Role;
+}
+
+export interface LoginDTO {
+  email: string;
+  senha: string;
+}
+
+export interface AuthResponseDTO {
+  token: string;
+  userId: number;
+  role: Role;
+}
+
+export interface CriarPratoDTO {
+  nome: string;
+  cozinha: string;
+  descricaoResumida: string;
+  descricaoDetalhada: string;
+  preco: number;
+  imagemUrl: string;
 }
